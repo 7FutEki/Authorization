66660a8aee2ac1038ca5d3d0da56e32cf5178e81
stud <stud@107-1>
2023-04-14 13:20:42 +0400

    Добавьте файлы проекта.

---
+++ Authorization/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿
+using Authorization.DB;
+using Microsoft.EntityFrameworkCore;
+using ModernWpf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Authorization
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            ApplicationContext db = new ApplicationContext();
+            ThemeManager.Current.ApplicationTheme = ApplicationTheme.Dark;
+            db.Database.Migrate();
+        }
+    }
+}
